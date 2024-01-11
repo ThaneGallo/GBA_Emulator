13c3842f5581ea6b1185016fa6810ac54c3238cc
ThaneGallo <124632962+ThaneGallo@users.noreply.github.com>
2024-01-11 16:23:27 -0500

    Update cartridge.c
    created lookup table for lic code

--- build/cartridge.c
+++ build/cartridge.c
@@ -1,50 +1,78 @@
 #include "cartridge.h"
 #include "common.h"
 
-FILE *fp;
-char cart_head[0x4F];
-struct rom_header rHead;
+typedef struct {
+	
+	u32 rom_size
+	u8 rom_data
+	rom_header *header
+	
+} cart;
 
-fp = fopen("","r");
 
-fgets(cart_head, 0x4F, fp);
-
-// for transferring the file contents into the struct for easier usage later
-
-for(int i = 0; i < 4, i++){
-rHead.entry[i] = cart_head[i]
+static const char *LIC_Code[0xA5]{
+	[0x00] = "None",
+	[0x01] = "Nintendo R&D1",
+	[0x08] = "Capcom",
+	[0x13] = "Electronic Arts",
+        [0x19] = "b-ai",
+	[0x20] = "kss",
+	[0x22] = "pow",
+	[0x24] = "PCM complete",
+	[0x25] = "san-x",
+	[0x28] = "Kemco Japan",
+	[0x29] = "seta",
+	[0x30] = "Viacom",
+	[0x31] = "Nintendo",
+	[0x32] = "Bandai",
+	[0x33] = "Ocean/Acclaim",
+	[0x34] = "Konami",
+	[0x35] = "Hector",
+	[0x37] = "Taito",
+	[0x38] = "Hudson",
+	[0x39] = "Banpresto",
+	[0x41] = "Ubisoft",
+	[0x42] = "atlas",
+	[0x44] = "Malibu",
+	[0x46] = "angel",
+	[0x47] = "Bullet-Proof",
+	[0x49] = "irem",
+	[0x50] = "Absolute",
+	[0x51] = "acclaim",
+	[0x52] = "Activision",
+	[0x53] = "American Sammy",
+	[0x54] = "Konami",
+	[0x55] = "Hi tech entertainment",
+	[0x56] = "LJN",
+	[0x57] = "Matchbox",
+	[0x58] = "Mattel",
+	[0x59] = "Milton Bradley",
+	[0x60] = "Titus",
+	[0x61] = "Virgin",
+	[0x64] = "LucasArts",
+	[0x67] = "Ocean",
+	[0x69] = "Electronic Arts",
+	[0x70] = "Infogrames",
+	[0x71] = "Interplay",
+	[0x72] = "Broderbund",
+	[0x73] = "sculpted",
+	[0x75] = "sci";
+	[0x78] = "THQ",
+	[0x79] = "Accolade",
+	[0x80] = "misawa",
+	[0x83] = "Iozc",
+	[0x86] = "Tokuma Shoten Intermedia",
+	[0x87] = "Tsukuda Original",
+	[0x91] = "Chunsoft",
+	[0x92] = "Video system",
+	[0x93] = "Ocean/Acclaim",
+	[0x95] = "Varie",
+	[0x96] = "Yonezawa/s'pal",
+	[0x99] = "Pack in soft",
+	[0xA4]= "Konami (Yu-Gi-Oh!)"
 }
 
-for(int i = 0; i < 0x30; i++){
-rHead.logo[i] = cart_head[0x4 + i];
+bool cart_load(char *cart){
+
 }
-
-for(int i = 0; i < 16; i++){
-rHead.title[i] = cart_head[0x34 + i];
-}
-
-rHead.cgb_flag = cart_head[0x43];
-
-rHead.new_lic_code = (cart_head[0x44] << 8) | cart_head[0x45];
-
-rHead.sgb_flag = cart_head[0x46];
-
-rHead.cart_type = cart_head[0x47];
-
-rHead.rom_size = cart_head[0x48];
-
-rHead.ram_size = cart_head[0x49];
-
-rHead.dest_code = cart_head[0x4A];
-
-//check later it says byte on pandocs but uses 2 bytes
-rHead.old_lic_code = cart_head[0x4B];
-
-rHead.version = cart_head[0x4C];
-
-rHead.checksum = ;
-
-rHead.global_checksum;
-
-
 

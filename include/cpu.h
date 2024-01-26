05dbf54468d125ada46df683c24ea704f04914c0
Thane <thanegallo@gmail.com>
2024-01-25 21:47:02 -0500

    added registers struct

--- include/cpu.h
+++ include/cpu.h
@@ -2,3 +2,19 @@
 
 #inclucde "common.h"
 
+typedef struct {
+    u8 a;
+    u8 f;
+    u8 b;
+    u8 c;
+    u8 d;
+    u8 e;
+    u8 h;
+    u8 l;
+    u16 pc;
+    u16 sp;
+
+} cpu_registers;
+
+void cpt_init();
+int cpu_step();

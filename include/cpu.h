914c917d29c5a6d3ea12a5092349142172b6f2ad
Thane <thanegallo@gmail.com>
2024-01-25 21:41:13 -0500

    created cpu.c and .h

---
+++ include/cpu.h
@@ -0,0 +1,4 @@
+#pragma once 
+
+#inclucde "common.h"
+
--- lib/cartridge.c
+++ lib/cartridge.c
@@ -186,7 +186,6 @@ bool cart_load(char *cart){
 u8 cart_read(u16 address){
 	//ROM ONLY type supported
 
-
 return ctx.rom_data[address];
 
 }
